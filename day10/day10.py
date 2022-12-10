2952e7c6344bbdb8f7c5c25d08d087c3566cfae6
Legendaren <Legendaren@users.noreply.github.com>
2022-12-10 14:07:26 +0100

    Day 10

--- day10/day10.py
+++ day10/day10.py
@@ -1,10 +1,7 @@
 from pathlib import Path
-import re
 
 
 class CPU:
-    addx_pattern = re.compile('^addx (-?\d+)$')
-    noop_pattern = re.compile('^noop$')
     required_cycles = {
         'addx': 2,
         'noop': 1
@@ -22,11 +19,10 @@ class CPU:
         return self.x_reg * self.cycle
 
     def current_instruction(self):
-        current_inst = self.instructions[self.instruction_counter]
-        if match := self.addx_pattern.findall(current_inst):
-            return "addx", int(match[0])
-        elif self.noop_pattern.findall(current_inst):
-            return "noop", None
+        current_inst = self.instructions[self.instruction_counter].split()
+        if len(current_inst) == 2:
+            return current_inst[0], int(current_inst[1])
+        return current_inst[0], None
 
     def execute(self):
         self.current_instruction_cycle += 1
@@ -41,8 +37,6 @@ class CPU:
         return self.instruction_counter >= len(self.instructions)
 
     def tick(self):
-        if self.is_finished():
-            return
         self.execute()
         self.cycle += 1
         self.signal_strengths.append(self.signal_strength())
@@ -70,10 +64,9 @@ def solve_part2():
     cpu = CPU(instructions)
     while not cpu.is_finished():
         sprite_pos = range(cpu.x_reg - 1, cpu.x_reg + 2)
-        crt_line.append('#' if pixel_pos in sprite_pos else '.')
+        crt_line.append('#' if pixel_pos % 40 in sprite_pos else '.')
         pixel_pos += 1
-        if pixel_pos > 39:
-            pixel_pos = 0
+        if pixel_pos % 40 == 0:
             crt.append(''.join(crt_line))
             crt_line.clear()
         cpu.tick()
