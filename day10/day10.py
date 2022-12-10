20e9cb8894960695ec9cf4f23eef1d5b761b75eb
Legendaren <Legendaren@users.noreply.github.com>
2022-12-10 13:41:52 +0100

    Day 10

--- day10/day10.py
+++ day10/day10.py
@@ -3,7 +3,17 @@ import re
 
 
 class CPU:
-    def __init__(self):
+    addx_pattern = re.compile('^addx (-?\d+)$')
+    noop_pattern = re.compile('^noop$')
+    required_cycles = {
+        'addx': 2,
+        'noop': 1
+    }
+
+    def __init__(self, instructions):
+        self.instructions = instructions
+        self.instruction_counter = 0
+        self.current_instruction_cycle = 0
         self.x_reg = 1
         self.cycle = 1
         self.signal_strengths = [self.x_reg * self.cycle]
@@ -11,18 +21,31 @@ class CPU:
     def signal_strength(self):
         return self.x_reg * self.cycle
 
-    def perform_cycle(self, n=1):
-        for _ in range(n):
-            self.cycle += 1
-            self.signal_strengths.append(self.signal_strength())
+    def current_instruction(self):
+        current_inst = self.instructions[self.instruction_counter]
+        if match := self.addx_pattern.findall(current_inst):
+            return "addx", int(match[0])
+        elif self.noop_pattern.findall(current_inst):
+            return "noop", None
 
-    def addx(self, n):
-        self.perform_cycle()
-        self.x_reg += n
-        self.perform_cycle()
+    def execute(self):
+        self.current_instruction_cycle += 1
+        op, arg = self.current_instruction()
+        if self.current_instruction_cycle >= self.required_cycles[op]:
+            if op == 'addx':
+                self.x_reg += arg
+            self.current_instruction_cycle = 0
+            self.instruction_counter += 1
 
-    def noop(self):
-        self.perform_cycle()
+    def is_finished(self):
+        return self.instruction_counter >= len(self.instructions)
+
+    def tick(self):
+        if self.is_finished():
+            return
+        self.execute()
+        self.cycle += 1
+        self.signal_strengths.append(self.signal_strength())
 
 
 def read_input():
@@ -33,16 +56,29 @@ def read_input():
 
 def solve_part1():
     instructions = read_input()
-    addx_pattern = re.compile('^addx (-?\d+)$')
-    noop_pattern = re.compile('^noop$')
-
-    cpu = CPU()
-    for inst in instructions:
-        if match := addx_pattern.findall(inst):
-            cpu.addx(int(match[0]))
-        elif noop_pattern.findall(inst):
-            cpu.noop()
+    cpu = CPU(instructions)
+    while not cpu.is_finished():
+        cpu.tick()
     return sum(cpu.signal_strengths[19:220:40])
 
 
+def solve_part2():
+    crt = []
+    crt_line = []
+    pixel_pos = 0
+    instructions = read_input()
+    cpu = CPU(instructions)
+    while not cpu.is_finished():
+        sprite_pos = range(cpu.x_reg - 1, cpu.x_reg + 2)
+        crt_line.append('#' if pixel_pos in sprite_pos else '.')
+        pixel_pos += 1
+        if pixel_pos > 39:
+            pixel_pos = 0
+            crt.append(''.join(crt_line))
+            crt_line.clear()
+        cpu.tick()
+    return '\n'.join(crt)
+
+
 print(f"Part 1 answer: {solve_part1()}")
+print(f"Part 2 answer: \n{solve_part2()}")
