0b53e478634d0359ec8ea8a0f42a632dee0b876d
Legendaren <Legendaren@users.noreply.github.com>
2022-12-11 15:49:21 +0100

    Day 11

--- day11/day11.py
+++ day11/day11.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from collections import deque
-import math
 import re
+import math
 
 
 class Monkey:
@@ -10,15 +10,18 @@ class Monkey:
                  starting_items,
                  operator_func,
                  divisor,
-                 true_monkey,
-                 false_monkey):
+                 if_true_monkey,
+                 if_false_monkey):
         self.n = n
         self.items = starting_items
         self.operator_func = operator_func
         self.divisor = divisor
         self.inspected_items_count = 0
-        self.true_monkey = true_monkey
-        self.false_monkey = false_monkey
+        self.if_true_monkey = if_true_monkey
+        self.if_false_monkey = if_false_monkey
+
+    def throw_to(self, is_divisible):
+        return self.if_true_monkey if is_divisible else self.if_false_monkey
 
     def popleft(self):
         self.inspected_items_count += 1
@@ -43,31 +46,29 @@ def parse_operation(op_str):
     return None
 
 
-def parse_test(divisible_by, if_true_monkey, if_false_monkey):
-    return lambda n: int(if_true_monkey) if n % int(divisible_by) == 0 else int(if_false_monkey)
-
-
 def read_input():
     p = Path(__file__).with_name("day11.txt")
+    with p.open() as f:
+        lines = f.read().split('\n\n')
+        lines = [line.split('\n') for line in lines]
+
     monkey_pattern = re.compile(r'Monkey (\d+)')
     starting_items_pattern = re.compile(r'(\d+)')
     operation_pattern = re.compile(r'Operation: new = (.+)')
     test_pattern = re.compile(r'Test: divisible by (\d+)')
     if_true_pattern = re.compile(r'If true: throw to monkey (\d+)')
     if_false_pattern = re.compile(r'If false: throw to monkey (\d+)')
-    with p.open() as f:
-        lines = f.read().split('\n\n')
-        lines = [line.split('\n') for line in lines]
-        monkeys = []
-        for line in lines:
-            n = int(monkey_pattern.findall(line[0])[0])
-            starting_items = deque(int(item) for item in starting_items_pattern.findall(line[1]))
-            operator_func = parse_operation(operation_pattern.findall(line[2])[0])
-            divisor = int(test_pattern.findall(line[3])[0])
-            true_monkey = int(if_true_pattern.findall(line[4])[0])
-            false_monkey = int(if_false_pattern.findall(line[5])[0])
-            monkeys.append(Monkey(n, starting_items, operator_func, divisor, true_monkey, false_monkey))
-        return monkeys
+
+    monkeys = []
+    for line in lines:
+        n = int(monkey_pattern.findall(line[0])[0])
+        starting_items = deque(int(item) for item in starting_items_pattern.findall(line[1]))
+        operator_func = parse_operation(operation_pattern.findall(line[2])[0])
+        divisor = int(test_pattern.findall(line[3])[0])
+        true_monkey = int(if_true_pattern.findall(line[4])[0])
+        false_monkey = int(if_false_pattern.findall(line[5])[0])
+        monkeys.append(Monkey(n, starting_items, operator_func, divisor, true_monkey, false_monkey))
+    return monkeys
 
 
 def solve_part1():
@@ -78,10 +79,10 @@ def solve_part1():
                 worry_level = monkey.popleft()
                 new_worry_level = monkey.operator_func(worry_level)
                 new_worry_level //= 3
-                monkey_to_throw_to = monkey.test_func(new_worry_level)
-                monkeys[monkey_to_throw_to].append(new_worry_level)
-    sorted_by_inspection_counts = sorted(monkeys, key=lambda item: item.inspected_items_count, reverse=True)
-    count1, count2 = [monkey.inspected_items_count for monkey in sorted_by_inspection_counts[:2]]
+                is_divisible = new_worry_level % monkey.divisor == 0
+                monkeys[monkey.throw_to(is_divisible)].append(new_worry_level)
+    sorted_by_inspection_counts = sorted(m.inspected_items_count for m in monkeys)
+    count1, count2 = sorted_by_inspection_counts[-2:]
     return count1 * count2
 
 
@@ -94,15 +95,12 @@ def solve_part2():
                 worry_level = monkey.popleft()
                 new_worry_level = monkey.operator_func(worry_level)
                 new_worry_level %= least_common_divisor
-                if new_worry_level % monkey.divisor == 0:
-                    monkeys[monkey.true_monkey].append(new_worry_level)
-                else:
-                    monkeys[monkey.false_monkey].append(new_worry_level)
-    sorted_by_inspection_counts = sorted(monkeys, key=lambda item: item.inspected_items_count)
-    count1, count2 = [monkey.inspected_items_count for monkey in sorted_by_inspection_counts[-2:]]
-    #return [m.inspected_items_count for m in monkeys]
+                is_divisible = new_worry_level % monkey.divisor == 0
+                monkeys[monkey.throw_to(is_divisible)].append(new_worry_level)
+    sorted_by_inspection_counts = sorted(m.inspected_items_count for m in monkeys)
+    count1, count2 = sorted_by_inspection_counts[-2:]
     return count1 * count2
 
 
-#print(f"Part 1 answer: {solve_part1()}")
+print(f"Part 1 answer: {solve_part1()}")
 print(f"Part 2 answer: {solve_part2()}")
